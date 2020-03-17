c85ee07ff3153b31e70fb32628eccbb612f785b7
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-03-16 19:17:59 -0500

    add details, add superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Finch
+from .models import Finch
 
 
-class Finch:
+class Finches:
   def __init__(self, name, breed, description, age):
     self.name = name
     self.breed = breed
@@ -16,14 +16,18 @@ finches = [
     Finch('Will', 'House', 'Grumpy Finch', 1),
     Finch('Kirby', 'Java', 'Happy little monster', 1),
 
-
 ]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>This is my finches</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+    finches = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {'finch': finch}) 
